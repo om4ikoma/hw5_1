066964608c61f7d8047defd3cbc4b679f5c97f4f
skoddrri <heavystarting@gmail.com>
2022-10-03 23:06:41 +0600

    deploy

--- handlers/inline.py
+++ handlers/inline.py
@@ -4,17 +4,17 @@ from aiogram import types, Dispatcher
 
 async def inline_google_handler(query: types.InlineQuery):
     text = query.query or "echo"
-    link = f"https://www.google.com/{text}"
+    link = f"https://www.google.com/search?q={text}"
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
     articles = [types.InlineQueryResultArticle(
-        id=hashlib.md5(text.encode()).hexdigest(),
+        id=result_id,
         title="Google: ",
         url=link,
         input_message_content=types.InputMessageContent(
             message_text=link
         )
     )]
-    await query.answer(articles, cache_time=60)
+    await query.answer(articles, cache_time=60, is_personal=True)
 
-
-def register_handlers_inline(dp: Dispatcher):
-    dp.register_inline_handler(inline_google_handler)
+    def register_handlers_inline(dp: Dispatcher):
+        dp.register_inline_handler(inline_google_handler)
